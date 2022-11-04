1f377645ffceb9a0eeae9e4b0c7eb16e821f34e6
Isaac Alter <i_alter@yahoo.com>
2022-11-04 12:48:27 -0600

    Adds first challnge completed

--- challenge1.rb
+++ challenge1.rb
@@ -3,7 +3,6 @@
 #GOAL: Look at an array of strings, and ONLY RETURN strings that have EXACTLY 4 characters.
 
 #1.) Make array of strings with varying ammounts of characters in them. MAKE SURE a number have 4.
-#2.) Make a method with a single parameter to store below code.
 #2.) Run array thru .each iterration. -- .each do
 #3.) if string char count is EXACTLY 4, then RETURN. What method can count chars on string? -- .length or .size? What is diff? Synonymous, I believe. Will use .length.
 #4.) puts the method using the array as an argument to check the returns.
@@ -12,10 +11,8 @@ test = ["food", "turkey", "red", "gasp", "You!", " no ", "   this should NOT be 
 
 #The returns I am looking for are: "food", "gasp", "You!", and " no ".
 
-def 4_char_return (array)
-    array.each do |element|
-        if element.length == 4
-            return element
-        end
-    end
-end
+#After some research, I found the .delete_if method which does exactl what I want!
+test.delete_if {|string| string.length != 4}
+
+#Printed it, to see if the return works, which it does!
+#p test
