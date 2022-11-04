1051b287e96b4153f3ed1afbc0507770a8a70766
Isaac Alter <i_alter@yahoo.com>
2022-11-04 11:56:48 -0600

    Initial commit

---
+++ challenge1.rb
@@ -0,0 +1,21 @@
+#CHALLENGE 1: Given an array of strings, return only the strings that have exactly 4 characters.
+
+#GOAL: Look at an array of strings, and ONLY RETURN strings that have EXACTLY 4 characters.
+
+#1.) Make array of strings with varying ammounts of characters in them. MAKE SURE a number have 4.
+#2.) Make a method with a single parameter to store below code.
+#2.) Run array thru .each iterration. -- .each do
+#3.) if string char count is EXACTLY 4, then RETURN. What method can count chars on string? -- .length or .size? What is diff? Synonymous, I believe. Will use .length.
+#4.) puts the method using the array as an argument to check the returns.
+
+test = ["food", "turkey", "red", "gasp", "You!", " no ", "   this should NOT be a returned VAlue!@#DSSDS#  3#ge ^^$  "]
+
+#The returns I am looking for are: "food", "gasp", "You!", and " no ".
+
+def 4_char_return (array)
+    array.each do |element|
+        if element.length == 4
+            return element
+        end
+    end
+end
