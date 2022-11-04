2f0ba430410d02dd47d56c4862b158c012b72ff9
Isaac Alter <i_alter@yahoo.com>
2022-11-04 13:31:46 -0600

    Adds third challnge completed

---
+++ challenge3.rb
@@ -0,0 +1,16 @@
+#CHALLENGE 3: Given an array of strings, return only the words that begin with the letter "t".
+
+#GOAL: RETURN not PRINT athe words in the array that begins with the letter "t", i.e. lowercase.
+
+#1.) Make an array of strings, MAKE SURE that there are strings that begin with "t"!
+#2.) Another .each iteration.
+#3.) Look for a method that will delete from arrays, which I beleive I found one in the first challenge! Even better, there's an opposite one! .keep_if
+#4.) The logic for the .keep_if will be if the string.chr == "t".
+#5.) Output the array to check the return.
+
+strings = ["This won't be here", "this will be here", "trains", "Trains", "stttttt", "Bussin", "t at the end"]
+
+strings.keep_if{|string| string.chr == "t"}
+
+#p strings
+#outputs correct! The code works!
